b55f0e124e07d9d03355cd93a6ab2990aa64dce9
lihop <leroy@leroy.geek.nz>
2014-12-04 17:05:06 +1300

    Add script to configure container at startup

---
+++ dovecot/configure.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# postfix configuration
+echo "mail.docker.container" > /etc/mailname
+cat /etc/postfix/master-additional.cf >> /etc/postfix/master.cf
+
+# configure mail delivery to dovecot
+cp /aliases /etc/postfix/virtual
+cp /domains /etc/postfix/virtual-mailbox-domains
+
+# todo: this could probably be done in one line
+mkdir /etc/postfix/tmp; awk < /etc/postfix/virtual '{ print $2 }' > /etc/postfix/tmp/virtual-receivers
+sed -r 's,(.+)@(.+),\2/\1/,' /etc/postfix/tmp/virtual-receivers > /etc/postfix/tmp/virtual-receiver-folders
+paste /etc/postfix/tmp/virtual-receivers /etc/postfix/tmp/virtual-receiver-folders > /etc/postfix/virtual-mailbox-maps
+
+# map virtual aliases and user/filesystem mappings
+postmap /etc/postfix/virtual
+postmap /etc/postfix/virtual-mailbox-maps
+
+# add user vmail who own all mail folders
+groupadd -g 5000 vmail
+useradd -g vmail -u 5000 vmail -d /srv/vmail -m
+chown -R vmail:vmail /srv/vmail
+chmod u+w /srv/vmail
+
+# Add password file
+RUN cp /passwords /etc/dovecot/passwd
--- dovecot/run.sh
+++ dovecot/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# configure things
+/configure.sh
+
 # start necessary services for operation (dovecot -F starts dovecot in the foreground to prevent container exit)
 chown -R vmail:vmail /srv/vmail
 service rsyslog start
